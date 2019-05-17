924ae83b3e1d1a0342c5dd608398e9a5221b6b95
Joel Göransson <joelggoransson@gmail.com>
2019-05-17 15:46:31 +0200

    initial commit

---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,25 @@
+int ledPin = 13;
+int state = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600);
+}
+void loop() {
+  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+ }
+ 
+ if (state == '0') {
+  digitalWrite(ledPin, LOW);
+  state = 0;
+ }
+ else if (state == '1') {
+  digitalWrite(ledPin, HIGH);  
+  state = 0;
+  
+ }else if (state == '2'){
+  Serial.print(digitalRead(ledPin));
+  state = 0;
+ }
+}
---
+++ website/website.py
@@ -0,0 +1,62 @@
+import bluetooth, subprocess
+from flask import Flask, render_template, redirect
+from flask_script import Manager, Server
+
+def connect():
+	nearby_devices = bluetooth.discover_devices(duration=4,lookup_names=True, flush_cache=True, lookup_class=False)
+	print("Which would would you like to connect to?")
+	for index, device in enumerate(nearby_devices, start=1):
+		print(str(index) + ":", device)
+
+	select = int(input("Select:" ))
+	print("Connecting to ", nearby_devices[select - 1][1])
+	global sock, bd_addr, port
+	bd_addr = nearby_devices[select - 1][0]
+	port = 1
+	sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	sock.connect((bd_addr,port))
+
+def led_on():
+	global sock
+	print("1 sent")
+	sock.send("1".encode())
+
+def led_off():
+	global sock
+	print("0 sent")
+	sock.send("0".encode())
+
+def get_status():
+	global sock
+	sock.send("2".encode())
+	print("SENT 2")
+	data = sock.recv(1).decode()
+	print("DATA:", data)
+	return data
+
+class CustomServer(Server):
+    def __call__(self, app, *args, **kwargs):
+        connect()
+        return Server.__call__(self, app, *args, **kwargs)
+
+app = Flask(__name__)
+manager = Manager(app)
+manager.add_command('runserver', CustomServer())
+
+@app.route('/')
+def index(name=None):
+	status = get_status()
+	return render_template("index.html", status=status, name=name)
+
+@app.route('/off/')
+def off(name=None):
+	led_off()
+	return redirect("/")
+
+@app.route('/on/')
+def on(name=None):
+	led_on()
+	return redirect("/")
+
+if __name__ == "__main__":
+    manager.run()
