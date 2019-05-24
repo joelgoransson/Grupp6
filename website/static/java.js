1801e3996ddb49f03577510e2f58ca44a45fb161
zaed89 <47356066+zaed89@users.noreply.github.com>
2019-05-24 14:16:41 +0200

    Update java.js

--- website/static/java.js
+++ website/static/java.js
@@ -1,3 +1,20 @@
 function showgrupp() {
     document.getElementById("Students").style.display = null;
 }
+var t = [];
+var i;
+spaning();
+
+function spaning() {
+  
+  var photo = document.getElementsByClassName("pic");
+  for (i = 0; i < photo.length; i++) {
+    photo[i].style.display = "none";  
+  }
+  t++
+  if (t > photo.length) 
+  {t = 1;}    
+  
+  photo[t-1].style.display = null;  
+  setTimeout(spaning, 4000);
+}
